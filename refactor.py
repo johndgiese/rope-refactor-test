aae07f953ab0d995ebf2b24832f0331a50e2178e
J. David Giese <johndgiese@gmail.com>
2016-01-24 13:05:59 -0500

    Get basic refactor working

--- refactor.py
+++ refactor.py
@@ -1,11 +1,13 @@
 import rope.base.project
 from rope.refactor.move import create_move
+from rope.base import libutils
 
-p = rope.base.project.Project('./exproject')
+p = rope.base.project.Project('/Users/david/Desktop/ropetest/exproject')
 
-source = p.get_resource('./exproject/a.py')
-dest = p.get_resource('./exproject/b.py')
+source = p.get_resource('a/b.py')
+dest = p.get_resource('.')
 
 m = create_move(p, source)
 
 p.do(m.get_changes(dest))
+p.close()
