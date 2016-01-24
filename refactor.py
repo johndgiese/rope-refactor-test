a6af94ad4e535ad9d03960594820568ab6d5c279
J. David Giese <johndgiese@gmail.com>
2016-01-24 14:22:15 -0500

    Make project path relative

--- refactor.py
+++ refactor.py
@@ -1,12 +1,13 @@
 import rope.base.project
 from rope.refactor.move import create_move
 
-p = rope.base.project.Project('/Users/david/Desktop/ropetest/exproject')
+if __name__ == '__main__':
+    p = rope.base.project.Project('exproject')
 
-source = p.get_resource('a/b.py')
-dest = p.get_resource('.')
+    source = p.get_resource('a/b.py')
+    dest = p.get_resource('.')
 
-m = create_move(p, source)
+    m = create_move(p, source)
 
-p.do(m.get_changes(dest))
-p.close()
+    p.do(m.get_changes(dest))
+    p.close()
